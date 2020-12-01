cf1b744f5baf32a51c1766dc1622c4903f66de99
Jamie Polackwich <rjpolackwich@gmail.com>
2020-12-01 05:51:03 -0700

    updating run_model to use custom dataloaders, adding relevant argparse args and moving to module runtime logic clause

--- model/run_model.py
+++ model/run_model.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
+
 from argparse import ArgumentParser
 import logging
-from models import ResnetClassifier
-import pytorch_lightning as pl
+
+from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.logging import TestTubeLogger
 import torch
 
+from transforms import build_transform
+from loaders import SkywayDataModule
+from models import SkywayMultiLabelClassifier
 
-logging.getLogger("botocore").setLevel(logging.CRITICAL)
 
-pl.seed_everything(777)
-model_map = {
-    'resnet': ResnetClassifier,
-}
+DEFAULT_BATCH_SIZE = 32
+DEFAULT_INPUT_IMAGE_SIZE = 256
+DEFAULT_LABELS_PATH = "/home/ubuntu/data/sample_sparse_encoings.csv"
+DEFAULT_IMAGES_DIR = "/home/ubuntu/data/chips"
+
+seed_everything(777)
 
 
 def save_model(model, fname):
@@ -22,29 +27,33 @@ def save_model(model, fname):
     torch.onnx.export(model, sample_input, fname, input_names=['input_batch'], output_names=['output_logits'])
 
 
-def main():
-    # parse args to get model
-    parser = ArgumentParser()
-    parser.add_argument('--model', type=str, default='resnet', help='only resnet is supported at this time')
-    temp_args, _ = parser.parse_known_args()
-    if temp_args.model not in model_map.keys():
-        raise ValueError('Invalid model; choose resnet')
-    model_cls = model_map[temp_args.model]
+def main(args):
+    datamod = SkywayDataModule(args.labels_path,
+                               args.images_dir,
+                               args.batch_size,
+                               )
 
-    # parse model args
-    parser = model_cls.add_model_specific_args(parser)
-    parser = pl.Trainer.add_argparse_args(parser)
-    args = parser.parse_args()
+    datamod.setup("train")
     model = model_cls(hparams=args)
 
     # Set up logger & trainer
     logger = TestTubeLogger(name="default", debug=False, save_dir="model_logs/")
-    trainer = pl.Trainer.from_argparse_args(args, logger=logger)
+    trainer = Trainer.from_argparse_args(args, logger=logger)
 
     # train and save model
-    trainer.fit(model)
+    trainer.fit(model, datamod)
     save_model(model, 'model.onnx')
 
 
 if __name__ == "__main__":
-    main()
+    parser = ArgumentParser()
+    parser = Trainer.add_argparse_args(parser)
+    parser = SkywayMultiLabelClassifier.add_model_specific_args(parser)
+    parser.add_argument('--batch-size', type=int, default=32, help='Training batch size')
+    parser.add_argument('--model-name', type=str, default='model.onnx', help='output ONNX model name')
+    parser.add_argument('--chip-size', type=int, default=DEFAULT_INPUT_IMAGE_SIZE, help='resize images to this size')
+    parser.add_argument('--labels-path', type=str, default=DEFAULT_LABELS_PATH, help='full path to sparse encoded labels csv file')
+    parser.add_argument('--images-dir', type=str, default=DEFAULT_IMAGES_DIR, help='full path to directory of image chip jpgs')
+
+    args = parser.parse_args()
+    main(args)
